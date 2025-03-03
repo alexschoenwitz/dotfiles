07ae8f96cfdd306606845122d6782efc4d299e8c
Alexandre Schönwitz <alexandre.schoenwitz@gmail.com>
2025-03-03 23:11:46 +0000

    fmt

--- nix/flake.nix
+++ nix/flake.nix
@@ -13,61 +13,59 @@
   };
 
   outputs = inputs@{ self, darwin, nixpkgs, mac-app-util, home-manager }:
-  let
-    configuration = { pkgs, config, ... }: {
-      environment.systemPackages =
-        [ 
-	# https://github.com/nix-community/home-manager/issues/1341
-	pkgs.mkalias # Find packages installed by nix in spotlight
-	pkgs.wezterm
-	pkgs.neovim
-	pkgs.brave
-	pkgs.git
+    let
+      configuration = { pkgs, config, ... }: {
+        environment.systemPackages = [
+          # https://github.com/nix-community/home-manager/issues/1341
+          pkgs.mkalias # Find packages installed by nix in spotlight
+          pkgs.wezterm
+          pkgs.neovim
+          pkgs.brave
+          pkgs.git
         ];
 
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
+        # Necessary for using flakes on this system.
+        nix.settings.experimental-features = "nix-command flakes";
 
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
+        # Set Git commit hash for darwin-version.
+        system.configurationRevision = self.rev or self.dirtyRev or null;
 
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 6;
+        # Used for backwards compatibility, please read the changelog before changing.
+        # $ darwin-rebuild changelog
+        system.stateVersion = 6;
 
-      system.defaults = {
+        system.defaults = {
           dock.autohide = true;
-	  loginwindow.GuestEnabled = false;
-	  NSGlobalDomain.AppleICUForce24HourTime = true;
-	  NSGlobalDomain.AppleInterfaceStyle = "Dark";
-	  NSGlobalDomain.KeyRepeat = 0;
-	  NSGlobalDomain.InitialKeyRepeat = 15;
-      };
+          loginwindow.GuestEnabled = false;
+          NSGlobalDomain.AppleICUForce24HourTime = true;
+          NSGlobalDomain.AppleInterfaceStyle = "Dark";
+          NSGlobalDomain.KeyRepeat = 0;
+          NSGlobalDomain.InitialKeyRepeat = 15;
+        };
 
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "aarch64-darwin";
+        # The platform the configuration will be used on.
+        nixpkgs.hostPlatform = "aarch64-darwin";
+        users.users.alexschoenwitz = {
+          name = "alexschoenwitz";
+          home = "/Users/alexschoenwitz";
+        };
 
-      users.users.alexschoenwitz = {
-        name = "alexschoenwitz";
-	home = "/Users/alexschoenwitz";
+        home-manager.users.alexschoenwitz = {
+          imports = [
+            ./home.nix # Create this file for your home-manager configuration
+          ];
+          home.stateVersion = "24.11"; # or your desired version
+        };
       };
-      home-manager.users.alexschoenwitz = {
-        imports = [
-          ./home.nix # Create this file for your home-manager configuration
+    in {
+      # Build darwin flake using:
+      # $ darwin-rebuild build --flake .#simple
+      darwinConfigurations."simple" = darwin.lib.darwinSystem {
+        modules = [
+          configuration
+          mac-app-util.darwinModules.default
+          home-manager.darwinModules.home-manager
         ];
-        home.stateVersion = "24.11"; # or your desired version
       };
     };
-  in
-  {
-    # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#simple
-    darwinConfigurations."simple" = darwin.lib.darwinSystem {
-      modules = [ 
-        configuration
-        mac-app-util.darwinModules.default
-	home-manager.darwinModules.home-manager
-      ];
-    };
-  };
 }
