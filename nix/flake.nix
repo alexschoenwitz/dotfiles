4ac87a7b8323bff33318f0331b369c13296a5f5b
Your Name <your.email@example.com>
2025-03-03 22:15:41 +0000

    dotfiles

---
+++ nix/flake.nix
@@ -0,0 +1,73 @@
+{
+  description = "nix-darwin system flake";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    darwin.url = "github:LnL7/nix-darwin/master";
+    mac-app-util.url = "github:hraban/mac-app-util";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    darwin.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ self, darwin, nixpkgs, mac-app-util, home-manager }:
+  let
+    configuration = { pkgs, config, ... }: {
+      environment.systemPackages =
+        [ 
+	# https://github.com/nix-community/home-manager/issues/1341
+	pkgs.mkalias # Find packages installed by nix in spotlight
+	pkgs.wezterm
+	pkgs.neovim
+	pkgs.brave
+	pkgs.git
+        ];
+
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
+
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 6;
+
+      system.defaults = {
+          dock.autohide = true;
+	  loginwindow.GuestEnabled = false;
+	  NSGlobalDomain.AppleICUForce24HourTime = true;
+	  NSGlobalDomain.AppleInterfaceStyle = "Dark";
+	  NSGlobalDomain.KeyRepeat = 0;
+	  NSGlobalDomain.InitialKeyRepeat = 15;
+      };
+
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "aarch64-darwin";
+
+      users.users.alexschoenwitz = {
+        name = "alexschoenwitz";
+	home = "/Users/alexschoenwitz";
+      };
+      home-manager.users.alexschoenwitz = {
+        imports = [
+          ./home.nix # Create this file for your home-manager configuration
+        ];
+        home.stateVersion = "24.11"; # or your desired version
+      };
+    };
+  in
+  {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#simple
+    darwinConfigurations."simple" = darwin.lib.darwinSystem {
+      modules = [ 
+        configuration
+        mac-app-util.darwinModules.default
+	home-manager.darwinModules.home-manager
+      ];
+    };
+  };
+}
---
+++ nix/home.nix
@@ -0,0 +1,103 @@
+{ config, pkgs, ... }:
+
+let
+  # Gruvbox theme for Oh-My-Zsh
+  gruvbox-zsh-theme = pkgs.fetchFromGitHub {
+    owner = "sbugzu";
+    repo = "gruvbox-zsh";
+    rev = "c54443c8d3da35037b7ae3ca73b30b45bc91a9e7";
+    sha256 = "sha256-pxG2PCw4hAgqu1T9DVjqdHM1t4g32B+N4URmAtoVdsU=";
+  };
+in
+{
+  home.packages = [
+    pkgs.zsh
+    pkgs.bat
+    pkgs.ripgrep
+  ];
+
+  programs = {
+    zsh = {
+      enable = true;
+      enableCompletion = true;
+      autosuggestion.enable = true;
+      syntaxHighlighting.enable = true;
+
+      oh-my-zsh = {
+        enable = true;
+        # Remove the theme property here
+        plugins = [
+          "git"
+          "sudo"
+        ];
+      };
+
+      initExtra = ''
+        # Source the gruvbox theme directly
+        source ${gruvbox-zsh-theme}/gruvbox.zsh-theme
+
+        # Set gruvbox theme configuration
+        SOLARIZED_THEME="dark"
+        GRUVBOX_DARK_PALETTE="dark"
+      '';
+    };
+
+    neovim = {
+      enable = true;
+      defaultEditor = true;
+      viAlias = true;
+      vimAlias = true;
+    };
+  };
+
+  # Custom themes need to be installed in the oh-my-zsh custom themes directory
+  xdg.configFile."oh-my-zsh/custom/themes/gruvbox.zsh-theme".source = "${gruvbox-zsh-theme}/gruvbox.zsh-theme";
+
+  # Add gruvbox theme for wezterm
+  xdg.configFile = {
+    "wezterm/wezterm.lua".text = ''
+      local wezterm = require 'wezterm'
+      local config = {}
+
+      config.color_scheme = 'GruvboxDark'
+      config.font = wezterm.font('JetBrains Mono')
+      config.font_size = 14
+
+      return config
+    '';
+
+    # Set up LazyVim
+    "nvim" = {
+      source = pkgs.fetchFromGitHub {
+        owner = "LazyVim";
+        repo = "starter";
+        rev = "main"; # You might want to pin this to a specific commit
+        sha256 = "sha256-QrpnlDD4r1X4C8PqBhQ+S3ar5C+qDrU1Jm/lPqyMIFM="; # Replace with the actual hash
+      };
+      recursive = true;
+    };
+
+    # Override LazyVim's default colorscheme to use gruvbox
+    "nvim/lua/plugins/colorscheme.lua".text = ''
+      return {
+        -- add gruvbox
+        { "ellisonleao/gruvbox.nvim" },
+        
+        -- Configure LazyVim to use gruvbox
+        {
+          "LazyVim/LazyVim",
+          opts = {
+            colorscheme = "gruvbox",
+          },
+        },
+      }
+    '';
+  };
+
+
+  programs.git = {
+    enable = true;
+    userName = "Alexandre Schönwitz";
+    userEmail = "alexandre.schoenwitz@gmail.com";
+  };
+}
