74c91d9a62f5a71b0fe218e47d426e0fbbb45aeb
Alexandre Schönwitz <alexandre.schoenwitz@gmail.com>
2025-03-04 00:20:19 +0000

    1password

--- nix/flake.nix
+++ nix/flake.nix
@@ -24,10 +24,10 @@
           home-manager.darwinModules.home-manager
           {
             # System configuration
-            nix.settings.experimental-features = [ "nix-command" "flakes" ];
+            nix.settings.experimental-features = ["nix-command" "flakes"];
             system.configurationRevision = self.rev or self.dirtyRev or null;
             system.stateVersion = 6;
-
+            
             # System defaults
             system.defaults = {
               dock.autohide = true;
@@ -39,23 +39,33 @@
                 InitialKeyRepeat = 15;
               };
             };
-
+            
             # System packages and settings
             nixpkgs.hostPlatform = system;
-            environment.systemPackages =
-              with nixpkgs.legacyPackages.${system}; [
-                wezterm
-                neovim
-                brave
-                git
+            environment.systemPackages = with nixpkgs.legacyPackages.${system}; [
+              wezterm
+              neovim
+              brave
+              git
+            ];
+            
+            homebrew = {
+              enable = true;
+              onActivation.autoUpdate = true;
+              onActivation.autoUpgrade = true;
+              onActivation.cleanup = "zap"; 
+              
+              casks = [
+                "1password"
               ];
-
+            };
+            
             # User configuration
             users.users.${username} = {
               name = username;
               home = "/Users/${username}";
             };
-
+            
             # Home Manager configuration
             home-manager = {
               useGlobalPkgs = true;
@@ -69,15 +79,16 @@
                   nil
                   nixfmt-classic
                   cargo
+                  _1password-cli
                 ];
-
+                
                 # Git config
                 programs.git = {
                   enable = true;
                   userName = "Alexandre Schönwitz";
                   userEmail = "alexandre.schoenwitz@gmail.com";
                 };
-
+                
                 # Zsh config with Gruvbox theme
                 programs.zsh = {
                   enable = true;
@@ -93,8 +104,7 @@
                       owner = "sbugzu";
                       repo = "gruvbox-zsh";
                       rev = "c54443c8d3da35037b7ae3ca73b30b45bc91a9e7";
-                      sha256 =
-                        "sha256-pxG2PCw4hAgqu1T9DVjqdHM1t4g32B+N4URmAtoVdsU=";
+                      sha256 = "sha256-pxG2PCw4hAgqu1T9DVjqdHM1t4g32B+N4URmAtoVdsU=";
                     };
                   in ''
                     source ${gruvbox-zsh-theme}/gruvbox.zsh-theme
@@ -102,7 +112,7 @@
                     GRUVBOX_DARK_PALETTE="dark"
                   '';
                 };
-
+                
                 # Neovim with LazyVim & Gruvbox
                 programs.neovim = {
                   enable = true;
@@ -110,7 +120,7 @@
                   viAlias = true;
                   vimAlias = true;
                 };
-
+                
                 # Configuration files
                 xdg.configFile = {
                   # Gruvbox theme for oh-my-zsh
@@ -119,11 +129,10 @@
                       owner = "sbugzu";
                       repo = "gruvbox-zsh";
                       rev = "c54443c8d3da35037b7ae3ca73b30b45bc91a9e7";
-                      sha256 =
-                        "sha256-pxG2PCw4hAgqu1T9DVjqdHM1t4g32B+N4URmAtoVdsU=";
+                      sha256 = "sha256-pxG2PCw4hAgqu1T9DVjqdHM1t4g32B+N4URmAtoVdsU=";
                     };
                   in "${gruvbox-zsh-theme}/gruvbox.zsh-theme";
-
+                  
                   # WezTerm with Gruvbox
                   "wezterm/wezterm.lua".text = ''
                     local wezterm = require 'wezterm'
@@ -135,19 +144,18 @@
 
                     return config
                   '';
-
+                  
                   # LazyVim configuration
                   "nvim" = {
                     source = pkgs.fetchFromGitHub {
                       owner = "LazyVim";
                       repo = "starter";
                       rev = "main";
-                      sha256 =
-                        "sha256-QrpnlDD4r1X4C8PqBhQ+S3ar5C+qDrU1Jm/lPqyMIFM=";
+                      sha256 = "sha256-QrpnlDD4r1X4C8PqBhQ+S3ar5C+qDrU1Jm/lPqyMIFM=";
                     };
                     recursive = true;
                   };
-
+                  
                   # Neovim plugins
                   "nvim/lua/plugins/colorscheme.lua".text = ''
                     return {
@@ -160,7 +168,7 @@
                       },
                     }
                   '';
-
+                  
                   "nvim/lua/plugins/lsp.lua".text = ''
                     return {
                       {
