780044af1d3ed1f6c23938c9745ada9ea4f2bf17
Alexandre Schönwitz <alexandre.schoenwitz@gmail.com>
2025-03-09 15:06:14 +0000

    p10k

--- modules/zsh/default.nix
+++ modules/zsh/default.nix
@@ -1,10 +1,10 @@
-{ ... }:
+{ pkgs, ... }:
 {
   programs.zsh = {
     enable = true;
     enableCompletion = true;
     syntaxHighlighting.enable = true;
     history.size = 10000;
-    initExtra = "source ~/.p10k.zsh";
+    initExtra = "source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme; source ~/.p10k.zsh";
   };
 }
