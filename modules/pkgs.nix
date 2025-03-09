649337913509ece8eb53ec2666238ceb5142f6f5
Alexandre Schönwitz <alexandre.schoenwitz@gmail.com>
2025-03-09 23:37:58 +0000

    cleanup

--- modules/pkgs.nix
+++ modules/pkgs.nix
@@ -1,12 +1,5 @@
 { pkgs, ... }:
 {
-  nixpkgs = {
-    config = {
-      allowUnfree = true;
-      allowUnfreePredicate = (_: true);
-    };
-  };
-
   home.packages =
     with pkgs;
     with pkgs.nodePackages_latest;
