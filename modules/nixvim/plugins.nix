e8f4e60f46b3815b7bd88b5aac030079b3fc5fab
Alexandre Schönwitz <alexandre.schoenwitz@freiheit.com>
2025-10-11 18:31:17 +0100

    update

--- modules/nixvim/plugins.nix
+++ modules/nixvim/plugins.nix
@@ -25,4 +25,48 @@
       };
     };
   };
+  trouble.enable = true;
+  tmux-navigator = {
+    enable = true;
+    keymaps = [
+      { action = "left"; key = "<C-w>h"; }
+      { action = "down"; key = "<C-w>j"; }
+      { action = "up"; key = "<C-w>k"; }
+      { action = "right"; key = "<C-w>l"; }
+      { action = "previous"; key = "<C-w>\\"; }
+    ];
+  };
+  snacks = {
+    enable = true;
+    settings = {
+      picker.enabled = true;
+      toggle = {
+        enabled = true;
+        settings = {
+          which-key = true;
+        };
+      };
+      explorer = {
+        enabled = true;
+      };
+    };
+  };
+  gitsigns = {
+    enable = true;
+    settings = {
+      current_line_blame = true;
+    };
+  };
+  lsp = {
+    enable = true;
+    inlayHints = true;
+  };
+  conform-nvim = {
+    enable = true;
+    settings = {
+      format_on_save = {
+        timeoutMs = 2000;
+      };
+    };
+  };
 }
