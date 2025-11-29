4f9f16ef0a9428f918d5d1adfa23ef2d6c8c5c1a
Alexandre Schönwitz <alexandre.schoenwitz@gmail.com>
2025-11-29 17:40:21 +0000

    claude-code

--- machines/home/default.nix
+++ machines/home/default.nix
@@ -4,4 +4,7 @@
   home-manager.users."alexandre.schoenwitz".programs.git.userEmail = "alexandre.schoenwitz@gmail.com";
   home-manager.users."alexandre.schoenwitz".programs.git.extraConfig.user.signingKey =
     "~/.ssh/id_ed25519";
+  home-manager.users."alexandre.schoenwitz".home.packages = [
+    pkgs.claude-code
+  ];
 }
