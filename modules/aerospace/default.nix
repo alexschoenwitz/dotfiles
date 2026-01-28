18997f42eb0904510045611c814388440edd90d5
Alexandre Schönwitz <alexandre.schoenwitz@gmail.com>
2026-01-28 23:58:14 +0000

    update

--- modules/aerospace/default.nix
+++ modules/aerospace/default.nix
@@ -3,4 +3,13 @@
   xdg.configFile."aerospace/aerospace.toml" = {
     source = config.lib.file.mkOutOfStoreSymlink ./aerospace.toml;
   };
+
+  launchd.agents.aerospace = {
+    enable = true;
+    config = {
+      ProgramArguments = [ "${config.home.homeDirectory}/.nix-profile/Applications/AeroSpace.app/Contents/MacOS/AeroSpace" ];
+      RunAtLoad = true;
+      KeepAlive = false;
+    };
+  };
 }
